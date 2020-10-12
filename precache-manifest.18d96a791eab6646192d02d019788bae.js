eca49c2a4be6ed50cb5fcd89ccb7d9eb89531079
repl.it user <replituser@example.com>
2020-10-12 00:59:15 +0000

    Updates

---
+++ precache-manifest.18d96a791eab6646192d02d019788bae.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4e3c502583fffab0f9c701ae04f71f",
+    "url": "/Master-Portfolio/index.html"
+  },
+  {
+    "revision": "f2a2e232296c56270b3b",
+    "url": "/Master-Portfolio/static/css/main.0ac01b03.chunk.css"
+  },
+  {
+    "revision": "135c339dad6a607bb82a",
+    "url": "/Master-Portfolio/static/js/2.57af0db3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Master-Portfolio/static/js/2.57af0db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a2e232296c56270b3b",
+    "url": "/Master-Portfolio/static/js/main.eababf9c.chunk.js"
+  },
+  {
+    "revision": "0ad9152b32b63c688789",
+    "url": "/Master-Portfolio/static/js/runtime-main.d4aa3358.js"
+  },
+  {
+    "revision": "a105ee341ae407dc0a16dcb77a5b78aa",
+    "url": "/Master-Portfolio/static/media/LibreBaskerville-Bold.a105ee34.ttf"
+  },
+  {
+    "revision": "9ecf41ba09bcd5e10c2524b2704ddc3f",
+    "url": "/Master-Portfolio/static/media/LibreBaskerville-Italic.9ecf41ba.ttf"
+  },
+  {
+    "revision": "228126bc49970c1f293e1bf96e8e2604",
+    "url": "/Master-Portfolio/static/media/LibreBaskerville-Regular.228126bc.ttf"
+  },
+  {
+    "revision": "6eb2ebe77be273e19995e36dc057afe7",
+    "url": "/Master-Portfolio/static/media/about.6eb2ebe7.svg"
+  },
+  {
+    "revision": "9dc176852559c83cd45cf3ea0c231057",
+    "url": "/Master-Portfolio/static/media/clip-work-searches.9dc17685.svg"
+  },
+  {
+    "revision": "f97efe3a54b9743c01bd3abf7b87e282",
+    "url": "/Master-Portfolio/static/media/home.f97efe3a.svg"
+  },
+  {
+    "revision": "6e78f3833120983dbc478d9390f7d11a",
+    "url": "/Master-Portfolio/static/media/icons8-adobe-illustrator.6e78f383.svg"
+  },
+  {
+    "revision": "cd0aa64871bcca37dda6d50b958828be",
+    "url": "/Master-Portfolio/static/media/icons8-adobe-photoshop.cd0aa648.svg"
+  },
+  {
+    "revision": "0b8f5028644f79c4952f463cad7c7d9f",
+    "url": "/Master-Portfolio/static/media/icons8-adobe-premiere-pro.0b8f5028.svg"
+  },
+  {
+    "revision": "889449e31190719bac49e10699663581",
+    "url": "/Master-Portfolio/static/media/icons8-css3.889449e3.svg"
+  },
+  {
+    "revision": "a644e06d98e3deb44f6e0dccacf1b0bb",
+    "url": "/Master-Portfolio/static/media/icons8-dev.a644e06d.svg"
+  },
+  {
+    "revision": "01e0b20c058cf041bc752dc5c7957345",
+    "url": "/Master-Portfolio/static/media/icons8-facebook.01e0b20c.svg"
+  },
+  {
+    "revision": "22106dd5aeb3fc03129d9b3c8ed8036e",
+    "url": "/Master-Portfolio/static/media/icons8-figma.22106dd5.svg"
+  },
+  {
+    "revision": "3b5d235109098e19f106a9d7c6e94c8f",
+    "url": "/Master-Portfolio/static/media/icons8-firebase.3b5d2351.svg"
+  },
+  {
+    "revision": "31602d701b8998f66248250b40c92db5",
+    "url": "/Master-Portfolio/static/media/icons8-flutter.31602d70.svg"
+  },
+  {
+    "revision": "f6050941083c77b1ec0b6753a1456dfa",
+    "url": "/Master-Portfolio/static/media/icons8-git.f6050941.svg"
+  },
+  {
+    "revision": "7697c1bf2fc67bdd49d7f50c4b34d1a1",
+    "url": "/Master-Portfolio/static/media/icons8-github.7697c1bf.svg"
+  },
+  {
+    "revision": "f5fe2a596c0a99d0001242592c494b62",
+    "url": "/Master-Portfolio/static/media/icons8-gitlab.f5fe2a59.svg"
+  },
+  {
+    "revision": "5e025b389b303c0d010a3294eb183d21",
+    "url": "/Master-Portfolio/static/media/icons8-html-5.5e025b38.svg"
+  },
+  {
+    "revision": "5b54675950638dd95de282256b5cf7c4",
+    "url": "/Master-Portfolio/static/media/icons8-javascript.5b546759.svg"
+  },
+  {
+    "revision": "d180dbd88d36cba75420ee46c8902681",
+    "url": "/Master-Portfolio/static/media/icons8-linkedin.d180dbd8.svg"
+  },
+  {
+    "revision": "cfee114b0b3de28827b98bee2ac0d6f0",
+    "url": "/Master-Portfolio/static/media/icons8-mongodb.cfee114b.svg"
+  },
+  {
+    "revision": "705b987e58fa317cf913c181018040be",
+    "url": "/Master-Portfolio/static/media/icons8-node.705b987e.svg"
+  },
+  {
+    "revision": "bbfecac08af0811c702e7711f3142f0c",
+    "url": "/Master-Portfolio/static/media/icons8-npm.bbfecac0.svg"
+  },
+  {
+    "revision": "1f6a76a4bf66ee1d6bdeab1edc5e2b5c",
+    "url": "/Master-Portfolio/static/media/icons8-react.1f6a76a4.svg"
+  },
+  {
+    "revision": "08e5938d67fcb582be7f2001c6181d16",
+    "url": "/Master-Portfolio/static/media/icons8-twitter.08e5938d.svg"
+  },
+  {
+    "revision": "5f906fcea09e08700c5b9706713851c5",
+    "url": "/Master-Portfolio/static/media/message.5f906fce.svg"
+  },
+  {
+    "revision": "a694ade0df318a9751c382cdb44a651f",
+    "url": "/Master-Portfolio/static/media/pixeltrue-error-1.a694ade0.svg"
+  },
+  {
+    "revision": "442b278844f3dacf968226aa509f82f2",
+    "url": "/Master-Portfolio/static/media/project.442b2788.svg"
+  },
+  {
+    "revision": "4a9c88185db22a91e514211855152f67",
+    "url": "/Master-Portfolio/static/media/skill.4a9c8818.svg"
+  },
+  {
+    "revision": "5bf185c782d6225fd07b2a3b66089bc4",
+    "url": "/Master-Portfolio/static/media/web-design.5bf185c7.svg"
+  },
+  {
+    "revision": "8c6d521578ba139353cc9fbfefd9aaad",
+    "url": "/Master-Portfolio/static/media/web-dev.8c6d5215.svg"
+  }
+]);
